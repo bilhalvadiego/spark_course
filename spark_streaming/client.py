cef23db81ff0ac9add65983f1733e384cb41a054
Diego Bilhalva <diego.bilhalva@gmail.com>
2023-01-17 08:53:06 -0300

    spark aula 2

--- spark_streaming/client.py
+++ spark_streaming/client.py
@@ -7,7 +7,9 @@ PORT = 3000
 s = socket.socket()
 s.connect((HOST, PORT))
 
-while True:
+controle = True
+while controle:
     data = s.recv(1024)
     print(data.decode("utf-8"))
     time.sleep(2)
+    controle = False if data.decode("utf-8") == 'fim' else True
